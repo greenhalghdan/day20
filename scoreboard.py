7ad1c7641a1726504cf57871350feabbd28f77cf
danie <greenhalghdan@gmail.com>
2023-05-31 19:38:55 +0100

    adding the font for the score

--- scoreboard.py
+++ scoreboard.py
@@ -3,18 +3,17 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.CURRENT_SCORE = 0
+        self.current_score = 0
         self.color("white")
-        self.goto(0, 280)
+        self.goto(0, 260)
         self.hideturtle()
         self.print_score()
 
-        #self.penup()
 
     def print_score(self):
         self.clear()
-        self.write(f"Current score is: {self.CURRENT_SCORE}", align="center")
+        self.write(f"Current score is: {self.current_score}", align="center", font=("times new roman", 24, "normal"))
 
 
     def update_score(self):
-        self.CURRENT_SCORE += 1
+        self.current_score += 1
