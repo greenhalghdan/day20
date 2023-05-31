776cc0931af3b0052ceb3d89aecdb884dde1f3b6
danie <greenhalghdan@gmail.com>
2023-05-31 20:14:15 +0100

    adding slicing

--- main.py
+++ main.py
@@ -31,15 +31,13 @@ while game_is_on:
         displayscore.print_score()
 
     #detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         displayscore.game_over()
 
     #collide with tail
-    for segment in snake.snake:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.snake[1:]:
+       if snake.head.distance(segment) < 10:
             game_is_on = False
             displayscore.game_over()
 
